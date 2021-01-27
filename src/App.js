83f3f59dd9cd7d9c306dcdf662030278f3081c9d
Juan Cruz Martinez <bajcmartinez@gmail.com>
2021-01-28 00:08:32 +0100

    Initial commit for the tutorial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { formatUserName } from './utils';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  // Load the data from the server
+  useEffect(() => {
+    let mounted = true;
+
+    const getUsers = async () => {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+      if (mounted) {
+        setUsers(response.data);
+      }
+    };
+
+    getUsers();
+
+    return () => { mounted = false; }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Users:</div>
+      {users.length ? (
+        <ul data-testid="user-list">
+          {users.map(user => (
+            <li key={user.id} className="user" data-testid="user-item">
+              <span>{user.name}</span> (<span>{formatUserName(user.username)}</span>)
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div>Loading users...</div>
+      )}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,42 @@
-import { render, screen } from '@testing-library/react';
+import axios from 'axios';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('axios');
+
+const fakeUsers = [{
+  "id": 1,
+  "name": "Test User 1",
+  "username": "testuser1",
+}, {
+  "id": 2,
+  "name": "Test User 2",
+  "username": "testuser2",
+}];
+
+
+describe('App component', () => {
+  test('it renders', async () => {
+    axios.get.mockResolvedValue({ data: fakeUsers });
+    render(<App />);
+
+    expect(screen.getByText('Users:')).toBeInTheDocument();
+  });
+
+  test('it displays a list of users', async () => {
+    axios.get.mockResolvedValue({ data: fakeUsers });
+
+    render(<App />);
+
+    const userList = await waitFor(() => screen.getByTestId('user-list'));
+    expect(userList).toBeInTheDocument();
+  });
+
+  test('it displays a row for each user', async () => {
+    axios.get.mockResolvedValue({ data: fakeUsers });
+    render(<App />);
+
+    const userList = await waitFor(() => screen.findAllByTestId('user-item'));
+    expect(userList).toHaveLength(2);
+  });
 });
